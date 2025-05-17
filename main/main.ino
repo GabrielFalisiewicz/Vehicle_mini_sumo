a0e853f06199cedb949a45c055edcf39dde34057
Gabriel <113853233+GabrielFalisiewicz@users.noreply.github.com>
2025-05-17 21:22:59 +0200

    WORK
    TESTED

--- main/main.ino
+++ main/main.ino
@@ -6,12 +6,11 @@
 #include "AsyncTCP.h"
 
 //AsyncWebServer
-AsyncWebServer server(80);
-AsyncWebSocket ws("/vs");
+AsyncWebServer server(1234);
+AsyncWebSocket ws("/ws");
 
 //Buffor PWM
 String buffor_data = "";
-char engine;
 int value_pwm;
 bool data_update = false;
 
@@ -25,18 +24,13 @@ PCF8574 PCF_01(0x20);
 #define PWM_A 2
 #define PWM_B 3
 
-//Konfiguracja WiFi
-const char* ssid = "vehicle_wifi_sumo";
-const char* password = "AtSumo01";
-const int channel = 6;
-//Samo urządzenie + diagnostyka
-const int max_users = 3; 
-const bool hidden_ssid = false;
-const int expower = 20; //dBm;
+//Obsługa czasu
+long current_time;
+long last_time = 0.0;
 
-IPAddress local_IP(192, 168, 5, 5);
-IPAddress gateway(255, 255, 255, 0);
-IPAddress sub(255, 255, 255, 0);
+//Konfiguracja WiFi (client mode)
+const char* ssid = "JF";
+const char* password = "wifiFJ11";
 
 //Parametry silników
 int max_value = 255;
@@ -44,16 +38,51 @@ int max_value = 255;
 //PWM Ferquency
 #define FERQ 5000
 
-void set_start_config_track(){
-    PCF_01.write(0, LOW);
-    PCF_01.write(1, HIGH);
-    PCF_01.write(2, HIGH);
-    PCF_01.write(3, LOW);
+void set_start_config_track() {
+    PCF_01.write(0, HIGH);
+    PCF_01.write(1, LOW);
+    PCF_01.write(2, LOW);
+    PCF_01.write(3, HIGH);
 }
 
-void change_direction_track(int first_pin, int sec_pin){
-   PCF_01.toggle(first_pin);
-   PCF_01.toggle(sec_pin);
+void change_direction_track(int first_pin, int sec_pin) {
+    PCF_01.toggle(first_pin);
+    PCF_01.toggle(sec_pin);
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+    switch (event) {
+        case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+            Serial.println("Połączono z WiFi.");
+            break;
+        case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+            Serial.print("Adres IP: ");
+            Serial.println(WiFi.localIP());
+            break;
+        case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+            Serial.println("Rozłączono z WiFi, ponawianie połączenia...");
+            WiFi.reconnect();
+            break;
+        default:
+            break;
+    }
+}
+
+void onWSEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+        Serial.println("WB connect");
+    } else if (type == WS_EVT_DISCONNECT) {
+        Serial.println("WB disconnect");
+    } else if (type == WS_EVT_DATA) {
+        buffor_data = "";
+        for (size_t i = 0; i < len; i++) buffor_data += (char)data[i];
+        Serial.println("Wiadomość: ");
+        value_pwm = buffor_data.toInt();
+        Serial.println(value_pwm);
+        value_engine_a += value_pwm;
+        value_engine_b += value_pwm;
+        data_update = true;
+    }
 }
 
 void setup() {
@@ -65,71 +94,41 @@ void setup() {
     pinMode(PWM_A, OUTPUT);
     pinMode(PWM_B, OUTPUT);
     set_start_config_track();
-    WiFi.softAP(ssid, password, channel, hidden_ssid, max_users);
-    WiFi.softAPConfig(local_IP, gateway, sub);
-    WiFi.setTxPower((wifi_power_t)expower);
+
     WiFi.onEvent(WiFiEvent);
-    
+
+    WiFi.begin(ssid, password);
+    Serial.println("Łączenie z WiFi");
+
+    if (!WiFi.status() != WL_CONNECTED) {
+        Serial.println("Błąd Łączenia WiFi");
+        delay(500);
+    }
+
+    // Inicjalizacja WebSocket
     ws.onEvent(onWSEvent);
     server.addHandler(&ws);
     server.begin();
-}
-
-void set_engine(int Pin){
-     if(value_pwm > max_value){
-          value_pwm = max_value;
-      }
-      if(Pin == PWM_A){
-        value_engine_a = value_pwm;
-      }
-      if(Pin == PWM_B){
-        value_engine_b = value_pwm;
-      }
-      analogWrite(Pin, value_pwm);
-      data_update = false;
+    IPAddress ip = WiFi.localIP();
+    Serial.println(ip);
 }
 
 void loop() {
-    if(data_update){
-        switch(engine){
-          case 'A':
-          set_engine(PWM_A);
-          break;
-          case 'B':
-          set_engine(PWM_B);
-          break;
-        }
-    } else {
-       if(value_engine_a > 0){
-          value_engine_a -= 30;
-          analogWrite(PWM_A, value_engine_a);
-       }
-       if(value_engine_b > 0){
-         value_engine_b -= 30;
-         analogWrite(PWM_B, value_engine_b);
-       }
+    current_time = millis();
+    if (data_update) {
+        Serial.println("Przypisane");
+        Serial.println(value_engine_a);
+        Serial.println(value_engine_b);
+        analogWrite(PWM_A, value_engine_a);
+        analogWrite(PWM_B, value_engine_b);
+        data_update = false;
     }
-}
-
-void WiFiEvent(WiFiEvent_t event){
-    switch(event){
-      case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
-      Serial.println("Nawiązano połączenie");
-      break;
+    if(current_time - last_time > 3000 && !data_update && value_engine_a > 0 && value_engine_b > 0){
+        last_time = current_time;
+        value_engine_a -= 10;
+        value_engine_b -= 10;
+        analogWrite(PWM_A, value_engine_a);
+        analogWrite(PWM_B, value_engine_b);
     }
-   
-}
-
-void onWSEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len){
-    if(type == WS_EVT_CONNECT){
-      Serial.println("WB connect");
-    } else if(type == WS_EVT_DISCONNECT){
-      Serial.println("WB disconnect");
-    } else if(type == WS_EVT_DATA){
-      for(size_t i = 0; i < len; i++) buffor_data += (char)data[i];
-      Serial.println("Wiadomosc: ");
-      Serial.println(buffor_data);
-      sscanf(buffor_data.c_str(), "%c %d", &engine, &value_pwm);
-      data_update = true;
-    }
+  
 }
