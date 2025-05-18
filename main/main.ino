dcf6a90310f05fb97952029f77c97cc90fae9991
Gabriel <113853233+GabrielFalisiewicz@users.noreply.github.com>
2025-05-18 14:58:21 +0200

    Change to two websocket
    tested - worked 10/10

--- main/main.ino
+++ main/main.ino
@@ -7,7 +7,8 @@
 
 //AsyncWebServer
 AsyncWebServer server(1234);
-AsyncWebSocket ws("/ws");
+AsyncWebSocket ws1("/ws");
+AsyncWebSocket ws2("/wr");
 
 //Buffor PWM
 String buffor_data = "";
@@ -30,24 +31,24 @@ long last_time = 0.0;
 
 //Konfiguracja WiFi (client mode)
 const char* ssid = "JF";
-const char* password = ".";
+const char* password = "wifiFJ11";
 
 //Parametry silników
-int max_value = 255;
+int max_value = 250;
 
 //PWM Ferquency
 #define FERQ 5000
 
-void set_start_config_track() {
-    PCF_01.write(0, HIGH);
-    PCF_01.write(1, LOW);
-    PCF_01.write(2, LOW);
-    PCF_01.write(3, HIGH);
+
+
+void set_left_track(int a, int b){
+    PCF_01.write(0, a);
+    PCF_01.write(1, b);
 }
 
-void change_direction_track(int first_pin, int sec_pin) {
-    PCF_01.toggle(first_pin);
-    PCF_01.toggle(sec_pin);
+void set_right_track(int c, int d){
+    PCF_01.write(2, c);
+    PCF_01.write(3, d);
 }
 
 void WiFiEvent(WiFiEvent_t event) {
@@ -68,20 +69,48 @@ void WiFiEvent(WiFiEvent_t event) {
     }
 }
 
-void onWSEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+
+
+void onWSEvent1(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
     if (type == WS_EVT_CONNECT) {
-        Serial.println("WB connect");
+        Serial.println("WB connect left");
+        delay(100);
+        ws1.textAll("led_01");
     } else if (type == WS_EVT_DISCONNECT) {
         Serial.println("WB disconnect");
     } else if (type == WS_EVT_DATA) {
         buffor_data = "";
         for (size_t i = 0; i < len; i++) buffor_data += (char)data[i];
-        Serial.println("Wiadomość: ");
-        value_pwm = buffor_data.toInt();
-        Serial.println(value_pwm);
-        value_engine_a += value_pwm;
-        value_engine_b += value_pwm;
-        data_update = true;
+        int first_search = buffor_data.indexOf(" ");
+        String message = buffor_data.substring(0, first_search);
+        value_pwm = buffor_data.substring(first_search + 1, buffor_data.length()).toInt();
+        Serial.println(message);
+        Serial.println(value_engine_a);
+        if(message.equals("left_pad")){
+          value_pwm = map(value_pwm, -100, 100, -255, 255);
+          value_engine_a = value_pwm;
+        }
+    }
+}
+
+void onWSEvent2(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+        Serial.println("WB connect right");
+        delay(100);
+        ws2.textAll("led_04");
+    } else if (type == WS_EVT_DISCONNECT) {
+        Serial.println("WB disconnect");
+    } else if (type == WS_EVT_DATA) {
+        buffor_data = "";
+        for (size_t i = 0; i < len; i++) buffor_data += (char)data[i];
+        int first_search = buffor_data.indexOf(" ");
+        String message = buffor_data.substring(0, first_search);
+        value_pwm = buffor_data.substring(first_search + 1, buffor_data.length()).toInt();
+         Serial.println(message);
+        if(message.equals("right_pad")){
+          value_pwm = map(value_pwm, -100, 100, -255, 255);
+          value_engine_b = value_pwm;
+        }
     }
 }
 
@@ -93,7 +122,9 @@ void setup() {
     analogWriteResolution(8);
     pinMode(PWM_A, OUTPUT);
     pinMode(PWM_B, OUTPUT);
-    set_start_config_track();
+
+    set_left_track(1, 0);
+    set_right_track(0, 1);
 
     WiFi.onEvent(WiFiEvent);
 
@@ -106,29 +137,47 @@ void setup() {
     }
 
     // Inicjalizacja WebSocket
-    ws.onEvent(onWSEvent);
-    server.addHandler(&ws);
+    ws1.onEvent(onWSEvent1);
+    server.addHandler(&ws1);
+    ws2.onEvent(onWSEvent2);
+    server.addHandler(&ws2);
     server.begin();
     IPAddress ip = WiFi.localIP();
     Serial.println(ip);
 }
 
-void loop() {
-    current_time = millis();
-    if (data_update) {
-        Serial.println("Przypisane");
-        Serial.println(value_engine_a);
-        Serial.println(value_engine_b);
-        analogWrite(PWM_A, value_engine_a);
-        analogWrite(PWM_B, value_engine_b);
-        data_update = false;
+void set_config_engines(int value_a, int value_b){
+
+    if(value_a <= 0){
+        set_left_track(0, 1);
+    } else {
+        set_left_track(1, 0);
     }
-    if(current_time - last_time > 3000 && !data_update && value_engine_a > 0 && value_engine_b > 0){
+
+    if(value_b <= 0){
+       set_right_track(1, 0);
+    } else {
+       set_right_track(0, 1);
+    }
+
+    if(value_a < 0){
+        value_a *= (-1);
+      
+    }
+    if(value_b < 0){
+        value_b *= (-1);
+    }
+
+    analogWrite(PWM_A, value_a);
+    analogWrite(PWM_B, value_b);
+}
+
+void loop() {
+   
+    current_time = millis();
+    set_config_engines(value_engine_a, value_engine_b);
+    if(current_time - last_time > 3000){
         last_time = current_time;
-        value_engine_a -= 10;
-        value_engine_b -= 10;
-        analogWrite(PWM_A, value_engine_a);
-        analogWrite(PWM_B, value_engine_b);
     }
   
 }
